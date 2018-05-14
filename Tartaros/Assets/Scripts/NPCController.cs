28da40008072680b5993c3c191321d449bcd7fc2
Peschcal <lauxpascal@web.de>
2018-05-15 01:08:55 +0200

    Added PlayerController and NpcController

---
+++ Tartaros/Assets/Scripts/NPCController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCController : MonoBehaviour {
+
+    // Use this for initialization
+
+    public Dialogue dialogue;
+
+    public static bool inRange = false;
+    //bool conversationStarted = false;
+
+
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		//if(inRange == true && conversationStarted == false)
+  //      {
+  //          if (Input.GetButtonDown("Fire3"))
+  //          {
+   //             conversationStarted = true;
+
+        //    }
+        //}
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+       
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Gespräch startet");
+           inRange = true;
+            TriggerDialogue();
+
+        }
+    }
+
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        
+        if (other.CompareTag("Player"))
+        {
+            
+            EndDialogue();
+            inRange = false;
+        }
+    }
+
+    public void TriggerDialogue()
+    {
+        
+            FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+
+    }
+
+    public void EndDialogue()
+    {
+        //conversationStarted = false;
+        //inRange = false;
+        FindObjectOfType<DialogueManager>().EndDialogue();
+    }
+}
---
+++ Tartaros/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private CharacterController controller;
+
+    private float speed = 10f;
+    private float verticalVelocity;
+    private float gravity = 30.0f;
+    private float jumpForce = 15f;
+
+    public DialogueManager dManager;
+
+
+    private float climbingSpeed = 10;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(verticalVelocity);
+        if (Input.GetButton("Horizontal"))
+        {
+            transform.Translate(new Vector3(Input.GetAxis("Horizontal") * speed, 0, 0));
+            Debug.Log("Horizontal");
+        }
+        if (Input.GetButton("Vertical"))
+        {
+            transform.Translate(new Vector3(0, 0, Input.GetAxis("Vertical") * speed));
+            Debug.Log("Vertical");
+        }
+
+        //if (Input.GetButton("Jump"))
+        //{
+        //    Debug.Log("Jump");
+        //}
+
+
+        //if (Input.GetButton("Fire1"))
+        //{
+        //    Debug.Log("Fire1");
+        //}
+
+        // if (Input.GetButton("Transform"))
+        // {
+        //    Debug.Log("Transform");
+        // }
+
+        if (NPCController.inRange == true)
+        {
+            if (Input.GetButtonDown("Fire3"))
+            {
+                dManager.DisplayNextSentence();
+            }
+        }
+
+        if (controller.isGrounded)
+        {
+            verticalVelocity = -gravity * Time.deltaTime;
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                verticalVelocity = jumpForce;
+            }
+        }
+        else
+        {
+            verticalVelocity -= gravity * Time.deltaTime;
+        }
+
+        Vector3 moveVector = Vector3.zero;
+
+        moveVector.x = Input.GetAxis("Horizontal") * speed;
+        moveVector.y = verticalVelocity;
+        moveVector.z = Input.GetAxis("Vertical") * speed;
+
+        controller.Move(moveVector * Time.deltaTime);
+
+
+
+
+
+    }
+}
