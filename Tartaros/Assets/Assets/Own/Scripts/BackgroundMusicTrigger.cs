9d6a6e3558cc26616df1a9534b6a48f4859cadd6
Peschcal <lauxpascal@web.de>
2018-08-23 02:26:25 +0200

    Started working on top part of first level, more models for different pipes, background music, new boots

---
+++ Tartaros/Assets/Assets/Own/Scripts/BackgroundMusicTrigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusicTrigger : MonoBehaviour {
+
+    public GameObject audio;
+
+    private AudioSource aSource;
+
+	// Use this for initialization
+	void Start () {
+        aSource = audio.GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (!aSource.isPlaying)
+            {
+                aSource.Play();
+
+            }
+        }
+    }
+}
---
+++ Tartaros/Assets/Assets/Own/Scripts/PressureSwitchController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressureSwitchController : MonoBehaviour
+{
+
+
+    public GameObject door;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Swiotch");
+            door.SetActive(false);
+        }
+    }
+}
